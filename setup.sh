b93a5b1a9d09fe1ab920e663cf999d541bdae413
kushagrasoni <soni.kushagra24@gmail.com>
2023-06-05 00:56:46 -0400

    Added additional pdf files to input

---
+++ setup.sh
@@ -0,0 +1,31 @@
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.11
+python3.11 --version
+which python3
+python3 --version
+python3.11 --version
+pip3.11 install --upgrade pip
+pip3 --version
+sudo apt install python3.11-pip
+sudo apt install python3-pip
+pip3 --version
+python3.11 -m pip install --upgrade pip
+python3.11 -m venv venv311_pvt_gpt
+ls -ltr
+rm -rf venv311_pvt_gpt/
+pwd
+cd ~
+ls -ltr
+sudo apt install python3.11-venv
+python3.11 -m venv venv311_pvt_gpt
+source venv311_pvt_gpt/bin/activate
+which pip
+pip install fastapi uvicorn
+pip install -U bitsandbytes git+https://github.com/huggingface/transformers.git
+pip install -U git+https://github.com/huggingface/peft.git git+https://github.com/huggingface/accelerate.git langchain
+pip install -U sentence_transformers pypdf qdrant-client
+ls -ltr
+git clone https://github.com/kushagrasoni/private_gpt_backend.git
